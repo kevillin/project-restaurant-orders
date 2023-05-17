87c6e49819512c7a26bed90ca0e950865cc203fd
Kevillin Santos <kevillin_santos@hotmail.com>
2023-05-17 16:57:04 -0300

    feat: requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    return []
+    ingredient = Ingredient("farinha")
+    ingredient2 = Ingredient("queijo parmesão")
+
+    assert ingredient.__hash__() == "farinha"
+    assert ingredient.__hash__() != "finha"
+    assert ingredient.__repr__() == "Ingredient('farinha')"
+    assert ingredient.__repr__() != "Ingreient('farnha')"
+
+    assert ingredient != ingredient2

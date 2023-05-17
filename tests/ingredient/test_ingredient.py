e8b0a760631b8dd0a8aa49caf34fbfc3290a7592
Kevillin Santos <kevillin_santos@hotmail.com>
2023-05-17 17:10:43 -0300

    feat: Requisito 1 concluido

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,14 +1,22 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
     ingredient = Ingredient("farinha")
     ingredient2 = Ingredient("queijo parmesão")
+    expected_restrictions = {Restriction.GLUTEN}
 
-    assert ingredient.__hash__() == "farinha"
-    assert ingredient.__hash__() != "finha"
+    assert ingredient.name == "farinha"
+    assert ingredient.name != "finha"
+    assert ingredient == Ingredient("farinha")
+    assert ingredient != Ingredient("queijo parmesão")
+    assert hash(ingredient) == hash(Ingredient("farinha"))
     assert ingredient.__repr__() == "Ingredient('farinha')"
     assert ingredient.__repr__() != "Ingreient('farnha')"
 
     assert ingredient != ingredient2
+    assert ingredient.__repr__() != ingredient2.__repr__()
+    assert ingredient.__hash__() != ingredient2.__hash__()
+
+    assert ingredient.restrictions == expected_restrictions
